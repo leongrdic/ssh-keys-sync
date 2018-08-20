439ce6d356079e782df1ae96a9bca0322c2e849b
Leon <leongrdic@gmail.com>
2018-08-20 16:20:18 +0200

    added a linux installation script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ -z "$(which php)" ]; then
+  echo 'error: php required but not found, exiting'
+  exit 1
+fi
+
+if [ -z "$(which git)" ]; then
+  echo 'error: git required but not found, exiting'
+  exit 2
+fi
+
+dir="$HOME/.ssh/sync"
+mkdir -p "$dir"
+cd "$dir"
+
+git clone https://github.com/leongrdic/ssh-keys-sync.git
+mv ssh-keys-sync/* .
+rm -rf ssh-keys-sync
+
+php setup.php
+
+echo 'setup done, running the first update to confirm the config is valid'
+
+php update.php
+if [ "$?" == 0 ]; then
+  echo 'success!'
+else
+  echo 'updating failed, please check the config; exiting'
+  exit 3
+fi
+
+echo -n 'how often (in minutes) do you want to sync the keys (1-60): '
+read minutes
+
+echo 'installing new cron job'
+
+cronjob="*/$minutes * * * * $(which php) $dir/update.php"
+
+crontab -l > tmp-crontab
+echo "$cronjob" >> tmp-crontab
+crontab tmp-crontab
+rm tmp-crontab
+
+echo "done"
